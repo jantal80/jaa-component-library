abed9a1c3dfc4d070c562f7fa947b7f3d4e25324
jantal80 <98295344+jantal80@users.noreply.github.com>
2024-10-28 14:59:09 -0400

    refactor makes theme work

--- lib/components/JaaButton/JaaButton.tsx
+++
@@ -1,9 +0,0 @@
-import Button from '@mui/material/Button';
-
-
-export function JaaButton() {
-
-    return (
-        <Button>JaaButton</Button>
-    )
-}
--- lib/main.ts
+++
@@ -1,8 +0,0 @@
-import { JaaButton } from './components/JaaButton/JaaButton'
-import JaaTable from './components/JaaTable/JaaTable'
-
-export { Button } from './components/Button'
-export { Input } from './components/Input'
-export { Label } from './components/Label'
-export { JaaButton }
-export { JaaTable }
